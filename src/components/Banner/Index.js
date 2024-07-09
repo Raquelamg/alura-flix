90fc2edc29a4c9f589160fb9614ce840ca4f0d66
Raquel <raquelmoreno_g@hotmail.com>
2024-07-09 16:50:13 -0600

    run build

--- src/components/Banner/Index.js
+++ src/components/Banner/Index.js
@@ -10,7 +10,7 @@ function Banner({img}){
             <h3 className={styles.textoReact}>Este challenge es una forma de aprendizaje. Es un mecanismo donde podrás comprometerte en la resolución de un problema para poder aplicar todos los conocimientos adquiridos en la formación React.</h3>
             </div>
             
-             <iframe className={styles.video} width="647.79px;" height="333.58px" src="https://www.youtube.com/embed/ov7vA5HFe6w?si=XTyNAGSOBAxb76gW" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+             <iframe className={styles.video} width="647.79px;" height="333.58px" src="https://www.youtube.com/embed/ov7vA5HFe6w?si=XTyNAGSOBAxb76gW" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
             
 
         </div>
